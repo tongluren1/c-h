3dd74fec3b05cf5c7d97db0c739c0f2dc63c65cb
guozheng wang <30545425+tongluren1@users.noreply.github.com>
2020-08-25 10:21:58 +0800

    -

--- jianshu/user_list_by_like.py
+++ jianshu/user_list_by_like.py
@@ -32,7 +32,7 @@ def spider(url, db):
     html = browser.page_source
     box = re.compile(box_pattern, re.I).findall(html)
 
-    print('num:' + len(box))
+    print(len(box))
     if len(box) > 1:
         box = re.compile(box_pattern, re.I).findall(html)
         for box_item in box:
