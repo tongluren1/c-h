27fc340060e69aa8b303ec73eb7e3b53161e0828
guozheng wang <30545425+tongluren1@users.noreply.github.com>
2020-10-26 17:01:38 +0800

    -

--- jianshu/article_info.py
+++ jianshu/article_info.py
@@ -65,13 +65,8 @@ def spider(url, db, article):
             sql = "insert into jianshu_article_info (%s) values (%s)" % (column_str, value_str)
             try:
                 db.query(sql)
-                db.query(
-                    'update jianshu_user set SucArticles = IFNULL(SucArticles, 0) + 1 where id = ' + article['UID'])
-                db.query("update jianshu_article_list set CommentsNum = '"+tmp_list['CommentsCount']+"', Wordage = '"+tmp_list['Wordage']+"', TotalRewardsCount = '"+tmp_list['TotalRewardsCount']+"', DownvotesCount = '"+tmp_list['DownvotesCount']+"', FeaturedCommentsCount = '"+tmp_list['FeaturedCommentsCount']+"', PaidType = '"+tmp_list['PaidType']+"', LikeNum = '"+tmp_list['LikesCount']+"', NotebookId = '"+tmp_list['NotebookId']+"', ViewsCount = '"+tmp_list['ViewsCount']+"', LastUpdatedAt = '"+tmp_list['LastUpdatedAt']+"', FirstSharedAt = '"+tmp_list['FirstSharedAt']+"' where ID = " +
-                    article['ID'])
-                print("update jianshu_article_list set CommentsNum = '"+tmp_list['CommentsCount']+"', Wordage = '"+tmp_list['Wordage']+"', TotalRewardsCount = '"+tmp_list['TotalRewardsCount']+"', DownvotesCount = '"+tmp_list['DownvotesCount']+"', FeaturedCommentsCount = '"+tmp_list['FeaturedCommentsCount']+"', PaidType = '"+tmp_list['PaidType']+"', LikeNum = '"+tmp_list['LikesCount']+"', NotebookId = '"+tmp_list['NotebookId']+"', ViewsCount = '"+tmp_list['ViewsCount']+"', LastUpdatedAt = '"+tmp_list['LastUpdatedAt']+"', FirstSharedAt = '"+tmp_list['FirstSharedAt']+"' where ID = " +
-                    article['ID'])
-                print('update jianshu_user set SucArticles = IFNULL(SucArticles, 0) + 1 where id = ' + article['UID'])
+                db.query('update jianshu_user set SucArticles = IFNULL(SucArticles, 0) + 1 where id = ' + article['UID'])
+                db.query("update jianshu_article_list set CommentsNum = '"+tmp_list['CommentsCount']+"', Wordage = '"+tmp_list['Wordage']+"', TotalRewardsCount = '"+tmp_list['TotalRewardsCount']+"', DownvotesCount = '"+tmp_list['DownvotesCount']+"', FeaturedCommentsCount = '"+tmp_list['FeaturedCommentsCount']+"', PaidType = '"+tmp_list['PaidType']+"', LikeNum = '"+tmp_list['LikesCount']+"', NotebookId = '"+tmp_list['NotebookId']+"', ViewsCount = '"+tmp_list['ViewsCount']+"', LastUpdatedAt = '"+tmp_list['LastUpdatedAt']+"', FirstSharedAt = '"+tmp_list['FirstSharedAt']+"' where ID = " + article['AID'])
             except BaseException:
                 status = 'FAILED'
                 print('------------- error ------------')
